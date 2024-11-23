fc8516eda984093bd5995485ba1c95140c51e843
ViktorOnyedim <viktoronyedim@gmail.com>
2024-11-24 00:02:04 +0100

    implemented floor annotated function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic annotations - Floor"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Returns the floor of a given float.
+
+    Args:
+        n (float): the input float.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return math.floor(n)
