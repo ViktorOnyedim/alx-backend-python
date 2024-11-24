b500e6a5c994fa4abeaece11fa221b8f80195a9c
ViktorOnyedim <viktoronyedim@gmail.com>
2024-11-24 01:53:30 +0100

    Implemented sum_mixed_list function

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Complex types - mixed list
+"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int]) -> float:
+    """Returns the float sum of list of integers"""
+    return sum(mxd_lst)
