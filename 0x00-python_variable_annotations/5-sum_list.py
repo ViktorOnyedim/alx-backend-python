780e772dda413a89f602068fc5a3a39e8c19c0c5
ViktorOnyedim <viktoronyedim@gmail.com>
2024-11-24 01:39:30 +0100

    implemented sum_list function

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Complex types - list of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Returns sum of input list elements"""
+    return sum(input_list)
